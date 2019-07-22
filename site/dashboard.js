c832eb0f9e2dc4896ea810b64f3f077451d97867
Monero Jeanniton <dev.mnr.j@gmail.com>
2019-07-22 17:24:33 -0400

    SEMA Dashboard Update: - Major UI Improvement - Change appropriate term for <Profit Sharing> - Fix timelin not showing all days of the months - Add 10% margin on seconday Y axis (from goal or current volume, which ever is higher) Improvement Needed: - Dispaly daily summary of kiosk Volumen and HTG - Progressively show profit sharing among on a second axis - Better track dashboard online status

--- site/dashboard.js
+++ site/dashboard.js
@@ -12,19 +12,13 @@ const params = {
 };
 
 // Initialize chart content
-const currentWaterVolume = {
+const currentPath = {
 			label: 'Volim Dlo Aktyèl',
 			backgroundColor: 'rgba(1, 1, 1, 0)',
 			borderColor: '#4471c4',
-			borderWidth: 5,
-			data: null
-		},
-		goal = {
-			label: 'Objektif',
-			backgroundColor: 'rgba(1, 1, 1, 0)',
-			borderColor: '#54b150',
-			borderWidth: 5,
-			data: [{x: 0,y: 88000}, {x: lastDayInMonth,y: 88000}]
+			borderWidth: 3,
+			radius: 3,
+			data: null,
 		},
 		goalPath = {
 			label: 'Wout Objektif',
@@ -32,90 +26,84 @@ const currentWaterVolume = {
 			borderColor: '#54b150',
 			borderWidth: 1,
 			borderDash: [10, 10],
-			data: [{x: 0,y: 0}, {x: lastDayInMonth,y: 88000}]
+			radius: 0,
+			// data: [{x: 0,y: 0}, {x: lastDayInMonth,y: 88000}]
+			data: null,
 		},
-		bar = {
+		goalBar = {
+			label: 'Objektif',
+			backgroundColor: 'rgba(1, 1, 1, 0)',
+			borderColor: '#54b150',
+			borderWidth: 2,
+			radius: 0,
+			// [{x: 0,y: 88000}, {x: lastDayInMonth,y: 88000}]
+			data: null,
+		},
+		profitBar = {
 			label: 'Objektif Minimum',
 			backgroundColor: 'rgba(1, 1, 1, 0)',
 			borderColor: '#facb35',
-			borderWidth: 5,
-			data: [{x: 0,y: 62000}, {x: lastDayInMonth,y: 62000}]
+			borderWidth: 2,
+			radius: 0,
+			// [{x: 0,y: 62000}, {x: lastDayInMonth,y: 62000}]
+			data: null,
+		},
+		// Define a padding from the goal or current (which ever is higher)
+		marginBar = {
+			label: '', // Y margin
+			backgroundColor: 'rgba(1, 1, 1, 0)',
+			borderColor: '#E0E0E0',
+			borderWidth: 1,
+			radius: 0,
+			showLine: false,
+			// [{x: 0,y: 62000}, {x: lastDayInMonth,y: 62000}]
+			data: null,
 		};
 
 $(window).on("load", function() {
 
-	const urlParams = new URLSearchParams(location.search);
-
-	if (!urlParams.has('k') && !urlParams.has('kiosk')) {
-		confirm("Pa gen kyòsk nan lyen an. SVP, mete yon kyòsk konsa: .../?k=cabaret");
-	} else {
-		params.siteName = urlParams.get('k') || urlParams.get('kiosk');
-		$('#kiosk-name').text(params.siteName);
-	}
-
-	let canvas = $("#canvas")[0],
-		ctx = canvas.getContext('2d');
-
-	// Configure the chart
-	Chart.defaults.global.elements.point.pointStyle = 'line';
-
-	// Setup our chart here
-	const chart = new Chart(ctx, {
-		// The type of chart we want to create
-		type: 'line',
-
-		// The data for our dataset
-		data: {
-			labels: daysInMonth,
-			datasets: [goal, goalPath, bar, currentWaterVolume]
-		},
-
-		// Configuration options go here
-		options: {}
-	});
-
-	// Refresh the date and time every second for live time
+	// Refresh the date and time every second for live time and as well as connectivity status
 	setInterval(refresh, 1000);
 
-	// Pull new data when ready
-	fetchDashboardData(params, chart)
-		.then(() => {
-			if (!navigator.onLine) {
-				$('#offline-status').show();
-			} else {
-				$('#offline-status').hide();
-			}
-		})
-		.catch(() => {
-			if (!navigator.onLine) {
-				$('#offline-status').show();
-			} else {
-				$('#offline-status').hide();
-			}
+	if(params.siteName = validateURL()){
+		// Update kiosk name on dashboard
+		$('#kiosk-name').text(params.siteName);
+
+		// Select the canvas for our chart
+		let canvas = $("#canvas")[0],
+			ctx = canvas.getContext('2d');
+
+		// Setup our chart here
+		Chart.defaults.global.elements.line.tension = 0.1;
+		const chart = new Chart(ctx, {
+			// The type of chart we want to create
+			type: 'line',
+
+			// The data for our dataset
+			data: {
+				labels: daysInMonth,
+				datasets: [goalBar, goalPath, profitBar, currentPath, marginBar]
+			},
+
+			// Configuration options go here
+			options: {}
 		});
 
-	// Pull new data regularly
-	setInterval(() => {
-		fetchDashboardData(params, chart)
-			.then(() => {
-				if (!navigator.onLine) {
-					$('#offline-status').show();
-				} else {
-					$('#offline-status').hide();
-				}
-			})
-			.catch(() => {
-				if (!navigator.onLine) {
-					$('#offline-status').show();
-				} else {
-					$('#offline-status').hide();
-				}
-			});
-	}, DEFAULT_SYNC_INTERVAL * 1000);
+		fetchDashboardData(params, chart);
+	}
+
 });
 
+// Refresh app locally
 function refresh() {
+	// Update the time and date
 	$("#date").html(getTimeAndDate());
+	// Update connectivity status
+	if (!navigator.onLine){
+		$('#offline-status').show();
+	}else{
+		$('#offline-status').hide();
+	}
 };
 
 // return current time and date
@@ -141,6 +129,17 @@ function getTimeAndDate() {
 	return ("<span class=\"date-data\">" + date + "</span> <span class=\"date-data\">" + time +"</span>");
 };
 
+// Valid URL contains kiosk parameter. isValid, return kiosk name. Otherwise, null
+function validateURL(){
+	const urlParams = new URLSearchParams(location.search),
+		kiosk = urlParams.get('k') || urlParams.get('kiosk');
+	// There is no kiosk parameter; invalid URL
+	if(!kiosk)
+		confirm("Pa gen kyòsk nan lyen an. SVP, mete yon kyòsk konsa: .../?k=cabaret");
+	return kiosk;
+}
+
+// Fetch kiosk number from SEMA
 function fetchDashboardData(params, chart) {
 	return new Promise((resolve, reject ) => {
 		let url = 'dataset/dashboard?siteName=' + params.siteName;
@@ -182,17 +181,21 @@ function fetchDashboardData(params, chart) {
 				});
 
 				// We update the bar values, the card values and update the chart
-				const goal = getSettingsValue(response.settings, 'monthly_goal')
-				const minGoal = getSettingsValue(response.settings, 'min_monthly_goal')
+				const goal = getSettingsValue(response.settings, 'monthly_goal'),
+					minGoal = getSettingsValue(response.settings, 'min_monthly_goal'),
+					marginY = Math.max(goal, latestVolume) * 1.1;
+
 
 				$('#water-volume').text(latestVolume);
 				$('#goal').text(goal);
 				$('#bonus').text(Number(parseFloat(calculateBonus(latestVolume, goal, minGoal)).toFixed(2)));
 
-				chart.data.datasets[3].data = newData;
+				// Index ordered as follow: [goal, goalPath, bar, currentPath, marginY]
 				chart.data.datasets[0].data = [{ x: 0,y: goal}, {x: lastDayInMonth,y: goal }];
-				chart.data.datasets[2].data = [{ x: 0,y: minGoal}, {x: lastDayInMonth,y: minGoal }];
 				chart.data.datasets[1].data = [{x: 0,y: 0}, {x: lastDayInMonth,y: goal}]
+				chart.data.datasets[2].data = [{ x: 0,y: minGoal}, {x: lastDayInMonth,y: minGoal }];
+				chart.data.datasets[3].data = newData;
+				chart.data.datasets[4].data = [{ x: 0,y: marginY}, {x: lastDayInMonth,y: marginY }];
 
 				chart.update();
 				resolve();
@@ -208,6 +211,7 @@ function fetchDashboardData(params, chart) {
 // If Bar < Total Volume < Goal, then Bonus = (Total Volume-Bar)/5
 // IF Total Volume>Goal, then Bonus = (Total Volume-Bar)/5 + (Total Volume-Goal)/5*2 + 1000
 function calculateBonus(currentVolume, goal, bar) {
+
 	if (currentVolume < bar) return 0;
 	else if (currentVolume < goal) return (currentVolume - bar) / 5;
 	else return ((currentVolume - bar) / 5) + ((currentVolume - goal) / (5 * 2)) + 1000;
@@ -226,9 +230,9 @@ function getDaysInMonth(month, year) {
 		daysTotal = new Date(year, month, 0).getDate(),
 		monthsList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 		daysList = [];
-	// Creating days list for the specified month
+	// Creating day list for the specified month
 	for(let i=0; i<daysTotal; i++){
-		daysList.push(`${monthsList[month]} ${i + 1}`);
+		daysList.push(`${monthsList[month - 1]} ${i + 1}`);
 	}
 	return daysList;
 
